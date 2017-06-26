571098ee211da5b706ada7daa6ce1da59b4bafee
Arthur Xavier Giffoni <arthur.giffoni@cedrofinances.com.br>
2017-06-26 18:05:07 -0300

    Blocos e Ordenação

---
+++ Ruby-Language-Documental/5 - Blocos e Ordenação.rb
@@ -0,0 +1,37 @@
+#=====================BLOCOS E ORDENAÇÃO================
+    #--METODOS, Blocos e Ordenação
+        #Def bloco
+        def welcome do
+            puts "Ola"
+        end
+
+        def add(x, y) do
+            x + y
+        end
+    
+        #chamada
+        welcome
+        add(1, 2)
+
+        #Caso o bloco tenha uma linha, Utilizar 
+            {}
+        #Caso contrario, utilizar 
+            do end
+
+        
+        #Ordenando
+        my_array = [3, 4, 8, 7, 1, 6, 5, 9, 2]
+        my_array.sort!
+            #Utilizado para ordenar qualquer tipo de "lista"
+        
+        #Comparação Combinado
+        <=>
+            # O operador de comparação combinado é assim: <=>. primeiro operando (item a ser comparado) 
+            # retorna 0 se o primeiro operando for igual ao segundo, 
+            # 1 se o primeiro operando for maior que o segundo, 
+            # e -1 se o primeiro operando for menor que o segundo.
+    
+    #
+#
+    
+#=========================================
